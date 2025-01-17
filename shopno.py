081dfcd061b767e83e7565b0ca41abdd1df70d17
chromaticv1 <101555574+chromaticv1@users.noreply.github.com>
2025-01-17 23:54:48 +0600

    added json output of raw data

--- shopno.py
+++ shopno.py
@@ -2,8 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+import json
+data = {"id":[], "product_name":[], "product_keyword":[], "loose":[], "price":[], }
+url_domain = 'https://www.shwapno.com/'
+url_subs = [
+    'dry-vegetables',
+     'eggs'
+    ]
 
-web = 'https://www.shwapno.com/eggs'
 path = './chromedriver-win64/chromedriver.exe'
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -11,16 +17,36 @@ service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service, options=options)
 
 
-driver.get(web)
+def scrape(catagory,should_remove_notification=False):
+    product_dataset = []
+    time.sleep(1)
+    if should_remove_notification:
+        notification = driver.find_element(By.CSS_SELECTOR,
+                                           "#headlessui-dialog-panel-\:r1\: > div.mt-3.border-t.pt-3.text-center > button.mx-2.inline-flex.justify-center.rounded-full.border-0.bg-darkLight.px-6.py-2.text-sm.font-medium.leading-none.text-black.outline-none.hover\:bg-darkLight")
+        notification.click()
+        time.sleep(.5)
 
-time.sleep(.5)
-notification = driver.find_element(By.CSS_SELECTOR, "#headlessui-dialog-panel-\:r1\: > div.mt-3.border-t.pt-3.text-center > button.mx-2.inline-flex.justify-center.rounded-full.border-0.bg-darkLight.px-6.py-2.text-sm.font-medium.leading-none.text-black.outline-none.hover\:bg-darkLight")
-notification.click()
-time.sleep(.2)
-products = driver.find_elements(By.CSS_SELECTOR, "div.product-box div.product-box-info")
-for product in products:
-    title = product.find_element(By.CSS_SELECTOR, "h2").text
-    price = product.find_element(By.CSS_SELECTOR, ".active-price").text
-    print(title, price)
+    products = driver.find_elements(By.CSS_SELECTOR, "div.product-box div.product-box-info")
+    i = 0
+    for product  in products:
+        product_data = {'id': i,
+                        'name': product.find_element(By.CSS_SELECTOR, "h2").text,
+                        'raw_price': product.find_element(By.CSS_SELECTOR, ".product-price").text}
+        if 'Unit' in product_data['raw_price']:
+            product_data['special_quantity'] = product.find_element(By.CSS_SELECTOR, "div.product-box-info div.product-box-attribute").text.split('\n')[0]
 
+        product_dataset.append(product_data)
+        i += 1
+    with open(f"raw_outputs/shopno-{catagory}.json", "w") as f:
+        f.write(json.dumps(product_dataset))
+
+
+for i, url_sub in enumerate(url_subs):
+    url = url_domain + url_sub
+    driver.get(url)
+    if i == 0:
+        scrape(url_sub,True)
+    else:
+        scrape(url_sub)
+    time.sleep(1.5)
 driver.quit()
