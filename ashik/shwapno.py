cc506d18c3de12339111b63ca7149a90008df233
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-18 20:41:02 +0600

    Adding file

--- ashik/shwapno.py
+++ ashik/shwapno.py
@@ -3,25 +3,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time 
 
-from functions.shwapno import clickingOnNotification, searchItems
-
 driver = webdriver.Chrome()
 
-shwapno = "https://www.shwapno.com/"
-driver.get(shwapno)
-time.sleep(1)
+URL = "https://www.shwapno.com/"
+driver.get(URL)
+time.sleep(5)
 
 # Notifications
-clickingOnNotification(driver)
+dialog = driver.find_element(By.ID, "headlessui-dialog-:r0:")
+inner_elements = dialog.find_elements(By.TAG_NAME, "button")
+inner_elements[1].click() # Clicking on No
 
+data = []
 productList = ["Rice", "Flour", "Lentil", "Soybean Oil", "Salt", "Egg", "Chicken", "Potato", "Eggplant","Onion", "Green Chilli"]
 
+def get_data(categoryName, startingIndex):
+  # Search for products
+  search_box = driver.find_element(By.CSS_SELECTOR, "#search-input")
+  search_box.clear()
+  search_box.send_keys(categoryName)
+  search_box.send_keys(Keys.ENTER)
 
-for item in productList:
-  main = driver.find_element(By.TAG_NAME, "body")
-  searchItems(main, item)
+  time.sleep(10)
 
 
+  main = driver.find_element(By.CSS_SELECTOR, "#product-grid")
+  products = main.find_elements(By.CSS_SELECTOR, "div.product-box div.product-box-info")
 
 
-time.sleep(10)
+  for product in products:
+    name = product.find_element(By.CSS_SELECTOR, "h2 > a").text
+    price = product.find_element(By.CSS_SELECTOR, "div.product-price > span.active-price").text
+    unit = product.find_element(By.CSS_SELECTOR, "div.product-price > span.font-normal.self-end").text
+    product_data = {"id": startingIndex, "name": name, "category": product, "price": price, "unit": unit}
+    data.append(product_data)
+    print(name, price, unit)
+    startingIndex += 1
+
+
+for category in productList:
+  get_data(category, len(data)+1)
+  time.sleep(5)
+  
+print(data)  
